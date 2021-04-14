41c3db826164839b8d9af83ca2b758421efe0996
Topsy-Goo <topsygoo@yandex.ru>
2021-04-14 19:06:52 +0300

    J3-7-ДЗ
    Reflection API и аннотации.

---
+++ server/src/main/java/ru/geekbrains/antonovdv/java3/lesson7/Order.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.antonovdv.java3.lesson7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.METHOD)
+public @interface Order
+{
+    int order() default 10;
+
+}// @interface Order
